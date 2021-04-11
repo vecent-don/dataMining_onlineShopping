698aa75e3f20a9bd33c8f854f37c0639c26995ac
vecent-don <181250127@smail.nju.edu.cn>
2021-04-11 15:33:32 +0800

    dev: can analysis a single sentence now

--- Util.py
+++ Util.py
@@ -3,58 +3,64 @@ import  re
 import datetime
 
 
-def usefulKey(s:str):
-    li=['IPADDR']
-
-def getDic(s:str):
-    infoList=str.split(" ")
-    #去掉‘【’
-    for i in range(2):
-        infoList[i]=preClean(infoList[i])
-
-
-
 
 def preClean(s:str):
-    s.strip()
-    if(len(s)<=0) or s[0]!='[':
+    s=s.strip()
+    if(len(s)<=0) or (s[0]!='[' and s[0]!='{'):
         return s
-    return s[0:len(s)-1].strip()
+    return s[1:len(s)-1].strip()
 
-
-
-
-def inUseList(s:str):
-    #处理IPADDR
-    dictionary={"IPADDR":["=","str"],}
 
 #getTime
-def regrex(s:str):
+def getTime(s:str):
     searchObj=re.search(r"[0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+",s)
     if searchObj:
         ans=searchObj.group()
         t=datetime.datetime.strptime(ans,'%Y-%m-%d %H:%M:%S')
-        print(t.timestamp())
-        print(ans)
-        return t
+        # print(t.timestamp())
+        # print(ans)
+        return "time",t
 
 #getUrl
 def getUrl(s:str):
     searchObj=re.search(r"uri.?=.?[a-zA-Z//]+",s)
     if(searchObj):
         ans=searchObj.group()
-        print(ans)
-        return ans
+        # print(ans)
+        return splitEqual(ans)
+
+def getIPADDR(s:str):
+
+    searchObj = re.search(r"IPADDR ?= ?([0-1]+\.){3}[0-1]+", s)
+    if(searchObj==None):
+        return None,None;
+    return splitEqual(searchObj.group())
 
 def getRequestBody(s:str):
-    searchObj=re.search(r"requestBody.?=.?\{.*\}",s)
-    candidate=preClean(re.search(r"\{.*\}",searchObj.group()).group()).split(",")
+
+    pattern=r"requestBody.?=.?\{.*\}"
+    # pattern2="requestBody.?=.?\{.*\}"
+    searchObj=re.search(pattern,s)
+    candidate=preClean(re.search(r"\{.*\}",searchObj.group()).group())
+    candidate=candidate.split(",")
     ans={}
     for i in candidate:
         key,value=getPair(i)
         ans[key]=value
-        print(key+" "+value)
+        #print(key+" "+value)
+    return "requestBody",ans
 
+def splitEqual(s:str):
+    s=s.split("=")
+    return preClean(s[0]),preClean(s[1])
+
+# def test(s:str):
+#     print(s)
+#     pattern=r"\\na"
+#     print(pattern)
+#     searchObject=re.search(pattern,s)
+#     print(searchObject.group())
+# test("\\na")
 
 def getPair(s:str):
     index=s.find(":")
@@ -62,9 +68,20 @@ def getPair(s:str):
         print("getPair: "+s+" is wronf\n")
         return s,""
     key=preClean(s[0:index])
+    key=key[1:len(key)-1]
     value=preClean(s[index+1:len(s)])
+    value=value[1:len(value)-1]
     return key,value
 
-s='got: [SESSIONID=9a183c12631dcde4cc5560a2599351ef] 2017-11-25 10:10:21 DEBUG [nio-8080-exec-1] com.some.taopao.aop.LogHandler : uri=/item/cart | requestBody={"userId" : "118675", "itemId" : "1668927", "categoryId" : "4444302"}';
-getRequestBody(s)
+def getDic(s:str):
+    ans={}
+    for i in [getUrl,getIPADDR,getTime,getRequestBody]:
+        key,value=i(s)
+        if(key!=None):
+            ans[key]=value
+    return ans
 
+
+s='got: [SESSIONID=9a183c12631dcde4cc5560a2599351ef] 2017-11-25 10:10:21 DEBUG [nio-8080-exec-1] com.some.taopao.aop.LogHandler : uri=/item/cart | requestBody={"userId" : "118675", "itemId" : "1668927", "categoryId" : "4444302"}';
+test=getDic(s)
+a=0
